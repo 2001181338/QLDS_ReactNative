d72a612f132d50b527d11cb38757b70f623525ee
Thin <thin.nguyen@nois.vn>
2021-11-04 22:56:13 +0700

    updated

--- Mobile/QLDS/App.js
+++ Mobile/QLDS/App.js
@@ -1,15 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
 import TabNavigation from './src/tab-navigation/tab-navigation';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import QuanLyDatVe from './src/dat-ve/quan-ly-dat-ve';
 import ChonGa from './src/dat-ve/chon-ga';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import QuanLyThongTin from './src/quan-ly-thong-tin/quan-ly-thong-tin';
-import { DatVe } from './src/dat-ve/dat-ve';
-const Tab = createBottomTabNavigator();
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
